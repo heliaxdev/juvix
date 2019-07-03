7cfda0cb79b5751f73b3759b74787629e9f96f4d
mariari <mariari@protonmail.com>
2019-07-03 05:46:57 -0400

    wrote the parser for bohm

--- src/Juvix/Bohm/Type.hs
+++ src/Juvix/Bohm/Type.hs
@@ -7,17 +7,20 @@ data Bohm
   = IntLit Int
   | Lambda SomeSymbol Bohm
   | Application Bohm Bohm
-  | Infix Op Bohm Bohm
+  | Infix' Op Bohm Bohm
   | Not Bohm
-  | True
-  | False
-  | Let SomeSymbol Bohm
+  | True'
+  | False'
+  | Letrec SomeSymbol Bohm
+  | Let SomeSymbol Bohm Bohm
   | If Bohm Bohm Bohm
   | Cons Bohm Bohm
   | Nil
   | Car Bohm
   | Cdr Bohm
+  | IsNil Bohm
   deriving Show
 
 
-data Op = Product | Plus | Division | Mod | Or | And | Eq | Neq | Lt | Gt | Ge | Le deriving Show
+data Op = Mult | Plus | Sub | Division
+        | Mod | Or | And | Eq | Neq | Lt | Gt | Ge | Le deriving Show
