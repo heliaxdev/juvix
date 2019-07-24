6eba19b58cd6615e7518e6ef3a175f6c0c345de4
mariari <mariari@protonmail.com>
2019-07-24 12:24:08 -0400

    progress on the mendler style of encoding!

--- experimental/playground/Mendler.hs
+++ experimental/playground/Mendler.hs
@@ -1,26 +1,57 @@
-import Juvix.Library hiding (foldM)
+{-# LANGUAGE ImpredicativeTypes #-}
+import Juvix.Library hiding (foldM, Nat)
 
--- attempt 1 : Church Encoding
-type ChurchNum a = (a -> a) -> a -> a
-newtype Church a = Church { unChurch :: ChurchNum a}
+-- Attempt 4 is a success!!!
 
--- attempt 2 : NatF ◂ ★ ➔ ★ = λ R: ★. ∀ X: ★. X ➔ (R ➔ X ➔ X) ➔ X.
-type NatF (r :: *) = forall (x :: *). x → (r → x → x) → x
-newtype Nat' a = Nat' {unNat :: NatF a}
-
--- attempt 3 : BB encoding/Church with extra!
-type NatBB a = forall b. (() → b) → (a → b) → b
-newtype NatBoehm a = Boehm {unBoehm :: NatBB a}
-
+-- Attempt 4 -------------------------------------------------------------------
 type AlgebraM (f :: * → *) (x :: *) = forall (r :: *). (r → x) → (f r) → x
-
-isEvenAlgM :: AlgebraM NatBoehm Bool
-isEvenAlgM rec' n = unBoehm n (\_ → True) (\n' →  not (rec' n'))
 
 type FixM f = forall x. AlgebraM f x → x
 
 foldM :: AlgebraM f x → FixM f → x
 foldM alg d = d alg
+
+-- Attempt 4 : Proper mendler style!
+data N r = Z | S r
+
+type Nat = FixM N
+
+
+in' :: ∀ (f :: * → *). f (FixM f) → FixM f
+in' r f = f (foldM f) r
+
+zero' :: AlgebraM N x -> x
+zero' = in' Z
+
+succ' n = in' (S n)
+
+two' :: AlgebraM N x -> x
+two' = succ' (succ' zero')
+
+three :: AlgebraM N x -> x
+three = succ' two'
+
+
+-- --predAlg :: AlgebraM N N
+-- predAlg _rec' n =
+--   case n of
+--     Z   → Z
+--     S n → n
+
+isEvenAlgN :: AlgebraM N Bool
+isEvenAlgN rec' n =
+  case n of
+    Z   → True
+    S n → not (rec' n)
+
+-- attempt 3 : BB encoding/Church with extra! ----------------------------------
+type NatBB a = forall b. (() → b) → (a → b) → b
+newtype NatBoehm a = Boehm {unBoehm :: NatBB a}
+
+
+isEvenAlgM :: AlgebraM NatBoehm Bool
+isEvenAlgM rec' n = unBoehm n (\_ → True) (\n' →  not (rec' n'))
+
 
 isEvenM :: FixM NatBoehm → Bool
 isEvenM = foldM isEvenAlgM
@@ -50,9 +81,16 @@ zero3 = Boehm (\ z _s → z ())
 one3 :: NatBoehm (NatBoehm a)
 one3 = Boehm (\ _z s → s zero3)
 
+-- attempt 2 : NatF ◂ ★ ➔ ★ = λ R: ★. ∀ X: ★. X ➔ (R ➔ X ➔ X) ➔ X. -------------
+type NatF (r :: *) = forall (x :: *). x → (r → x → x) → x
+newtype Nat' a = Nat' {unNat :: NatF a}
 -- Attempt 2 example
 zero2 :: Nat' a
 zero2 = Nat' (\z _s → z)
 
 one2 :: Nat' (Nat' a)
 one2 = Nat' (\z s → s zero2 z)
+
+-- attempt 1 : Church Encoding -------------------------------------------------
+type ChurchNum a = (a -> a) -> a -> a
+newtype Church a = Church { unChurch :: ChurchNum a}
