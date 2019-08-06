dbfb998828b94b7b2654ed6fd3231894b5c94fb1
mariari <mariari@protonmail.com>
2019-08-06 10:45:30 -0400

    Fixed the arch linux z3 bug, and made the evaluator properly generate constraints without making new vars which are not distinct

--- src/Juvix/Eal/Solve.hs
+++ src/Juvix/Eal/Solve.hs
@@ -9,27 +9,42 @@ import qualified Z3.Monad         as Z3
 import           Juvix.Eal.Types2
 import           Juvix.Library    hiding (link, reduce)
 
+
 runMultipleConstraints :: Int → [Constraint] → RPT → IO ()
 runMultipleConstraints numRepeat constraints syntax = do
   let numset = grabTermNumbers syntax mempty
-      recGen _               _         _    0   = pure ()
-      recGen constraintCall printModel cons num = do
-        (r, v, s) ← Z3.evalZ3 (constraintCall cons)
+      recGen _               _         _      0   = pure ()
+      recGen constraintCall printModel consZ3 num = do
+        (r, v, s) ← Z3.evalZ3 constraintCall
         printModel v
         putText "-->"
         print (r, s)
         case s of
-          Just x → do
+          Just x →
             let bounds  = filter (\(i,_) → Set.member i numset) (zip [1..] x)
                 newCons =
                   bounds
-                  >>| (\(i,x) → Constraint [ConstraintVar 1 i] (Eq (fromInteger x)))
-                extraConstraint = (makeVarMap newCons >>= flip constraintsToZ3 newCons >>= Z3.mkNot)
-            recNext extraConstraint (newCons <> cons) (pred num)
+                  >>| (\(i,x) → Constraint [ConstraintVar 1 i]
+                                           (Eq (fromInteger x)))
+                extraConstraint = makeVarMap newCons
+                                  >>= flip constraintsToZ3 newCons
+                                  >>= Z3.mkNot
+            in
+            recNext extraConstraint
+                    consZ3
+                    (pred num)
           Nothing → pure ()
-      recFirst      = recGen constraintSystem putStrLn
-      recNext extra = recGen (\c → extra >>= constraintSystemAnd c) (\_ → pure ())
-  recFirst constraints numRepeat
+
+      recFirst             = recGen (constraintSystem constraints)
+                                    putStrLn
+                                    (pure [])
+      recNext extra consZ3 = recGen (do
+                                        e   ← extra
+                                        z3s ← consZ3
+                                        constraintSystemAnd constraints (e : z3s))
+                                    (\_ → pure ())
+                                    ((:) <$> extra <*> consZ3)
+  recFirst numRepeat
   where
     -- Could use a list, since this should be ascending, but I do not assume that
     grabTermNumbers (RBang i (RLam _ t)) s = grabTermNumbers t (Set.insert i s)
@@ -39,7 +54,7 @@ runMultipleConstraints numRepeat constraints syntax = do
 
 runConstraints ∷ [Constraint] → IO ()
 runConstraints constraints = do
-  (r, v, s) ← Z3.evalZ3 (constraintSystem constraints)
+  (r, v, s)  ← Z3.evalZ3 (constraintSystem constraints)
   putStrLn v
   putText "-->"
   print (r, s)
@@ -74,16 +89,16 @@ constraintToZ3 varMap (Constraint vars op) = opToZ3 op =<< varsToZ3 varMap vars
 constraintsToZ3 ∷ Map.Map Int Z3.AST → [Constraint] → Z3.Z3 Z3.AST
 constraintsToZ3 varMap constraints = Z3.mkAnd =<< traverse (constraintToZ3 varMap) constraints
 
-constraintsToZ3Extra :: Z3.AST → Map Int Z3.AST → [Constraint] → Z3.Z3 Z3.AST
+constraintsToZ3Extra :: [Z3.AST] → Map Int Z3.AST → [Constraint] → Z3.Z3 Z3.AST
 constraintsToZ3Extra extra varMap constraints = do
   cons ← traverse (constraintToZ3 varMap) constraints
   and  ← Z3.mkAnd cons
-  Z3.mkAnd [and, extra]
+  Z3.mkAnd (and : extra)
 
 makeVarMap :: Z3.MonadZ3 f ⇒ [Constraint] → f (Map Int Z3.AST)
 makeVarMap constraints =
   let vars = Set.toList (collectVars constraints) in
-  traverse (\v → Z3.mkFreshIntVar ("m_" <> show v)
+  traverse (\v → (Z3.mkIntVar =<< Z3.mkStringSymbol ("m_" <> show v))
                  >>| (,) v)
            vars
   >>| Map.fromList
@@ -101,16 +116,17 @@ constraintSystemGen constraints modifyAst = do
     catMaybes <$> traverse (Z3.evalInt m) (Map.elems varMap)
   pure (res, model, sol)
 
-constraintSystemAnd ∷ [Constraint] → Z3.AST → Z3.Z3 (Z3.Result, GHC.Base.String, Maybe [Integer])
+constraintSystemAnd ∷ [Constraint] → [Z3.AST] → Z3.Z3 (Z3.Result, GHC.Base.String, Maybe [Integer])
 constraintSystemAnd c z3Constraint = constraintSystemGen c (constraintsToZ3Extra z3Constraint)
 
 constraintSystem ∷ [Constraint] → Z3.Z3 (Z3.Result, GHC.Base.String, Maybe [Integer])
 constraintSystem constraints = constraintSystemGen constraints constraintsToZ3
 
 -- wtf, this doesn't work
+-- it does now!
 computeTwo ∷ IO (Maybe Integer)
 computeTwo = Z3.evalZ3 $ do
-  x  ← Z3.mkFreshIntVar "x"
+  x  ← Z3.mkIntVar =<< (Z3.mkStringSymbol "x")
   _2 ← Z3.mkInteger 2
   Z3.assert =<< Z3.mkEq x _2
   fmap snd
