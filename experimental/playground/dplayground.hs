bb7531272ad0f6479e69708aa490dd6641dcccf4
Marty Stumpf <thealmartyblog@gmail.com>
2019-08-06 09:10:35 -0700

    iEval on plusZeroIsIdentity works. iType doesn't.

--- experimental/playground/dplayground.hs
+++ experimental/playground/dplayground.hs
@@ -164,7 +164,7 @@ iEval (EqElim a m mr x y eq)    d  =
 vapp :: Value -> Value -> Value
 vapp (VLam f)      v =  f v
 vapp (VNeutral n)  v =  VNeutral (NApp n v)
-vapp x             y =  error ("Cannot apply \n" ++ showVal y ++ "\n to \n" ++ showVal x)
+vapp x             y =  error ("Application (vapp) error. Cannot apply \n" ++ showVal y ++ "\n to \n" ++ showVal x)
 
 cEval :: CTerm -> Env -> Value
 cEval (Inf  ii)   d =  iEval ii d
@@ -416,6 +416,9 @@ plusZeroIsIdentityIMType =
     (cPi cNat
       (cPi (cEq cNat (cBound 1) (cBound 2))
       cStar))
+
+--Proof of x + 0 = x
+
 --Turn plusZeroIsIdentityIM from a CTerm to an ITerm.
 iplusZeroIsIdentityIM :: ITerm
 iplusZeroIsIdentityIM =
@@ -442,24 +445,21 @@ plusZeroIsIdentityIM =
 plusZeroIsIdentityInductive :: CTerm
 plusZeroIsIdentityInductive = 
   (Lam  --k :: Nat
-    (cEqElim
+    (Lam (cEqElim
       --1st argument: of type type, in this case Nat.
-      cNat
+      (cBound 0)
       --2nd argument: motive, takes in x, y, Eq a x y and returns a type,
       --in this case it's Eq Nat (plusZero (Succ k)) (Succ k) 
       plusZeroIsIdentityIM
-      --3rd argument, resulting type should be 
-      --for all z::Nat.Eq Nat (plusZero (Succ z)) (Succ z).
+      --3rd argument, resulting type should be Eq Nat (plusZero (Succ k)) (Succ k).
       (Lam (cRefl cNat (cSucc (cBound 0))))
       --4th argument, x, of type Nat
-      (Inf (plusZero :@: (cBound 0)))
+      (Inf (plusZero :@: (cBound 1)))
       --5th argument, y, of type Nat
-      (cBound 0)
+      (cBound 1)
       --6th argument, of type Eq a x y
-      (cRefl cNat (cBound 0))
-      )
-    )
---Proof of x + 0 = x
+      (cRefl cNat (cBound 1)))))
+
 plusZeroIsIdentity :: CTerm -> ITerm
 plusZeroIsIdentity = NatElim --point-free style, x is omitted
   --motive takes in x and returns the type Eq Nat (x+0) x, \x. Eq Nat (x + 0) x
