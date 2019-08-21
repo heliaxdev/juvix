003bbb4d9056f1c101c0b68e7b251bfd1d3bb63d
Marty Stumpf <thealmartyblog@gmail.com>
2019-08-21 12:27:13 -0700

    Add most ITerms to core and parser.

--- src/Juvix/Core/MainLang.hs
+++ src/Juvix/Core/MainLang.hs
@@ -2,45 +2,61 @@
 -- Atkey 2017 and McBride 2016.
 module Juvix.Core.MainLang where
 
-import           Prelude
 import           Numeric.Natural
+import           Prelude
+
+data NatAndw -- semiring of (Nat,w) for usage annotation
+  = Natural Natural -- 0, 1, or n usage
+  | Omega -- unspecified usage
+
+instance Show NatAndw where
+  show (Natural n) = show n
+  show Omega   = "w"
+
+instance Eq NatAndw where
+  Omega == _ = True
+  
+instance Num NatAndw where
+  x - y = x - y
+
+type Nat = Natural
 
 -- Inferable terms
 data ITerm
-  =  Star Natural             -- (sort i) i th ordering of (closed) universe.
-  |  Natural                  -- (Prim) primitive type
-  |  Pi Natural CTerm CTerm   -- formation rule of the dependent function type (PI). 
-                              -- the Natural(π) tracks how many times x is used.
-  |  Pm Natural CTerm CTerm   -- dependent multiplicative conjunction (tensor product)
-  |  Pa Natural CTerm CTerm   -- dependent additive conjunction type
-  |  NPm CTerm CTerm          -- non-dependent multiplicative disjunction type
-  |  Bound Natural            -- Bound variables, in de Bruijn indices
-  |  Free Name                -- Free variables of type name (see below)
-  |  App Natural ITerm CTerm  -- elimination rule of PI (APP).
-                              -- the Natural(π) tracks how x is use.
+  = Star Natural -- (sort i) i th ordering of (closed) universe.
+  | Nat Natural -- (Prim) primitive type
+  | Pi NatAndw CTerm CTerm -- formation rule of the dependent function type (PI).
+                              -- the NatAndw(π) tracks how many times x is used.
+  | Pm NatAndw CTerm CTerm -- dependent multiplicative conjunction (tensor product)
+  | Pa NatAndw CTerm CTerm -- dependent additive conjunction type
+  | NPm CTerm CTerm -- non-dependent multiplicative disjunction type
+  | Bound Natural -- Bound variables, in de Bruijn indices
+  | Free Name -- Free variables of type name (see below)
+  | App NatAndw ITerm CTerm -- elimination rule of PI (APP).
+                              -- the NatAndw(π) tracks how x is use.
   deriving (Show, Eq)
 
 --Checkable terms
 data CTerm
-  =  Inf  ITerm         --(CONV) conversion rule. TODO make sure 0Γ ⊢ S≡T
+  = Inf ITerm --(CONV) conversion rule. TODO make sure 0Γ ⊢ S≡T
                         -- Inf is the constructor that embeds ITerm to CTerm
-  |  Lam  Natural CTerm --(LAM) Introduction rule of PI.
-                        -- The abstracted variable's usage is tracked with the Natural(π).
+  | Lam NatAndw CTerm --(LAM) Introduction rule of PI.
+                        -- The abstracted variable's usage is tracked with the NatAndw(π).
   deriving (Show, Eq)
 
 data Name
-  =  Global  String -- Global variables are represented by name thus type string
-  |  Local   Natural    -- to convert a bound variable into a free one
-  |  Quote   Natural
+  = Global String -- Global variables are represented by name thus type string
+  | Local Natural -- to convert a bound variable into a free one
+  | Quote Natural
   deriving (Show, Eq)
 
 --Values/types
 data Value
-  = VLam (Value -> Value)
+  = VLam Value (Value -> Value)
   | VStar Natural
-  | VPi Natural Value (Value -> Value)
-  | VPm Natural Value (Value -> Value)
-  | VPa Natural Value (Value -> Value)
+  | VPi NatAndw Value (Value -> Value)
+  | VPm NatAndw Value (Value -> Value)
+  | VPa NatAndw Value (Value -> Value)
   | VNPm Value Value
   | VNeutral Neutral
 
@@ -50,56 +66,69 @@ data Neutral
   | NApp Neutral Value
 
 showVal :: Value -> String
-showVal (VLam _)      = "lambda"
-showVal (VStar i)     = "* " ++ show i
-showVal (VPi n v _f)  = "Pi " ++ show n ++ showVal v ++ " -> lambda"
+showVal (VLam _ f) = showFun f
+showVal (VStar i) = "*" ++ show i
+showVal (VPi n v f) = "[" ++ show n ++ "]" ++ showVal v ++ " -> " ++ showFun f
+showVal (VPm n v f) =
+  "([" ++ show n ++ "]" ++ showVal v ++ ", " ++ showFun f ++ ")"
+showVal (VPa _ _ _) = "/\\"
+showVal (VNPm _ _) = "\\/"
 showVal (VNeutral _n) = "neutral "
+
+showFun :: (Value -> Value) -> String
+showFun _f = "\\x.t"
 
 --vfree creates the value corresponding to a free variable
 vfree :: Name -> Value
 vfree n = VNeutral (NFree n)
 
 --Contexts map variables to their types.
-type Type    = Value
+type Type = Value
+
 type Context = [(Name, Type)]
 
 toInt :: Natural -> Int
 toInt = fromInteger . toInteger
-
+{-}
 --Evaluation
 type Env = [Value]
 
 iEval :: ITerm -> Env -> Value
-iEval (Star i)     _d  =  VStar i
-iEval (Pi n ty ty') d  =  VPi (n-1) (cEval ty d) (\ x -> cEval ty' (x : d))
-iEval (Pm n ty ty') d  =  VPm (n-1) (cEval ty d) (\ x -> cEval ty' (x : d))
-iEval (Pa n ty ty') d  =  VPa (n-1) (cEval ty d) (\ x -> cEval ty' (x : d))
-iEval (Free  x)    _d  =  vfree x
-iEval (Bound  ii)   d  =  d !! (toInt ii) --(!!) :: [a] -> Int -> a, the list lookup operator.
---iEval (App iterm cterm)   d  =  vapp (iEval iterm d) (cEval cterm d)
-iEval Natural       _  = undefined
-iEval (NPm  ty ty') d  =  undefined VNPm ty ty' d
+iEval (Star i) _d = VStar i
+iEval (Pi Omega ty ty') d = VPi Omega (cEval ty d) (\x -> cEval ty' (x : d))
+iEval (Pi n ty ty') d = VPi (n - 1) (cEval ty d) (\x -> cEval ty' (x : d))
+iEval (Pm n ty ty') d = VPm (n - 1) (cEval ty d) (\x -> cEval ty' (x : d))
+iEval (Pa n ty ty') d = VPa (n - 1) (cEval ty d) (\x -> cEval ty' (x : d))
+iEval (Free x) _d = vfree x
+iEval (Bound ii) d = d !! (toInt ii) --(!!) :: [a] -> Int -> a, the list lookup operator.
+iEval (App n iterm cterm) d = vapp (iEval iterm d) (cEval cterm d)
+iEval Nat _ = undefined
+iEval (NPm ty ty') d = undefined VNPm ty ty' d
 
 vapp :: Value -> Value -> Value
-vapp (VLam f)      v =  f v
-vapp (VNeutral n)  v =  VNeutral (NApp n v)
-vapp x             y =  error ("Application (vapp) error. Cannot apply \n" ++ showVal y ++ "\n to \n" ++ showVal x)
+vapp (VLam n f) v = f v
+vapp (VNeutral n) v = VNeutral (NApp n v)
+vapp x y =
+  error
+    ("Application (vapp) error. Cannot apply \n" ++
+     showVal y ++ "\n to \n" ++ showVal x)
 
 cEval :: CTerm -> Env -> Value
-cEval (Inf  ii)   d =  iEval ii d
---cEval (Lam  e)    d =  VLam (\ x -> cEval e (x : d))
+cEval (Inf ii) d = iEval ii d
 
+--cEval (Lam  e)    d =  VLam (\ x -> cEval e (x : d))
 --substitution function for inferable terms
 iSubst :: Natural -> ITerm -> ITerm -> ITerm
 iSubst ii r (Bound j)
-  | ii == j  = r
-  |otherwise = Bound j
-iSubst _ii _r (Free y)        =  Free y
+  | ii == j = r
+  | otherwise = Bound j
+iSubst _ii _r (Free y) = Free y
 --iSubst ii r (App iterm cterm) =
-iSubst _ii _r (Star i)        =  Star i
---iSubst ii r (Pi n ty ty')     =  Pi
+iSubst _ii _r (Star i) = Star i
 
+--iSubst ii r (Pi n ty ty')     =  Pi
 --substitution function for checkable terms
 cSubst :: Natural -> ITerm -> CTerm -> CTerm
-cSubst ii r (Inf e) =  Inf (iSubst ii r e)
+cSubst ii r (Inf e) = Inf (iSubst ii r e)
 --cSubst ii r (Lam e) =  Lam (cSubst (ii + 1) r e)
+-}
--- src/Juvix/Core/Parser.hs
+++ src/Juvix/Core/Parser.hs
@@ -1,134 +1,169 @@
 module Juvix.Core.Parser where
 
-     import           Prelude
-     import           Juvix.Core.MainLang
-     import           Text.Parsec
-     import           Text.ParserCombinators.Parsec
-     import           Text.ParserCombinators.Parsec.Language
-     import           Data.Functor.Identity
-     import qualified Text.ParserCombinators.Parsec.Token as Token
+import           Juvix.Core.MainLang     
+import           Data.Functor.Identity
+import           Prelude
+import           Text.Parsec
+import           Text.ParserCombinators.Parsec
+import           Text.ParserCombinators.Parsec.Language
+import qualified Text.ParserCombinators.Parsec.Token    as Token
 
-     languageDef ∷ GenLanguageDef String u Data.Functor.Identity.Identity
-     languageDef =
-          emptyDef { Token.commentStart    = "/*"
-               , Token.commentEnd      = "*/"
-               , Token.commentLine     = "//"
-               , Token.identStart      = letter
-               , Token.identLetter     = alphaNum
-               , Token.reservedNames   = [ "Pi", "App", --ITerms with CTerms as inputs
-                                           "Bound", --Bound var
-                                           "Free","Local","Quote","Global" --Free var
-                                         ]
-               , Token.reservedOpNames = [ "Inf", "Lam", ":",
-                                             ":@:" --application
-                                             ]
-               }
-     lexer ∷ Token.GenTokenParser String u Data.Functor.Identity.Identity
-     lexer = Token.makeTokenParser languageDef
+languageDef :: GenLanguageDef String u Data.Functor.Identity.Identity
+languageDef =
+  emptyDef
+    { Token.commentStart = "/*"
+    , Token.commentEnd = "*/"
+    , Token.commentLine = "//"
+    , Token.identStart = letter
+    , Token.identLetter = alphaNum
+    , Token.reservedNames =
+        [ "*" --sort
+        , "[Π]" --function type
+        , "[π]" --dependent multiplicative conjunction type
+        , "/\\" --dependent additive conjunction type
+        , "\\/" --non-dependent multiplicative disjunction type
+        , "Bound" --Bound var
+        , "Free"
+        , "Local"
+        , "Quote"
+        , "Global" --Free var
+        , "w" --Omega
+        ]
+    , Token.reservedOpNames = ["Inf", "\\", ":"]
+    }
 
+lexer :: Token.GenTokenParser String u Data.Functor.Identity.Identity
+lexer = Token.makeTokenParser languageDef
      -- These are parsers for what their names signify
-     identifier ∷ Parser String
-     identifier = Token.identifier lexer
 
-     --reserved ∷ String -> Parser ()
-     reserved   = Token.reserved lexer
+identifier :: Parser String
+identifier = Token.identifier lexer
 
-     --reservedOp ∷ String -> Parser ()
-     reservedOp = Token.reservedOp lexer
+reserved :: String -> Parser ()
+reserved = Token.reserved lexer
 
-     --parens ∷ Parser String -> Parser String
-     parens = Token.parens lexer -- parses surrounding parenthesis, and what is inside them
+reservedOp :: String -> Parser ()
+reservedOp = Token.reservedOp lexer
 
-     natural ∷ Parser Integer
-     natural = Token.natural lexer
+parens :: Parser String -> Parser String
+parens = Token.parens lexer -- parses surrounding parenthesis, and what is inside them
 
-     whiteSpace ∷ Parser ()
-     whiteSpace = Token.whiteSpace lexer
-     {-
-     --Parser for naturals.
-     nats ∷ Parser Natural
-     nats =  parens nats
-          <|> natural
+natural :: Parser Integer
+natural = Token.natural lexer
+
+whiteSpace :: Parser ()
+whiteSpace = Token.whiteSpace lexer
      
-     piTerm ∷ Parser ITerm
-     piTerm =
-       do reserved "Pi"
-          pi <- nats
-          input <- cterm
-          func <- cterm
-          return $ Pi input func
+natw ∷ Parser NatAndw
+natw =   do reserved "w"
+            return Omega
+     <|> do n <- natural
+            return $ Natural (fromInteger n) 
+            
+sortTerm :: Parser ITerm
+sortTerm =
+     do reserved "*"
+        n <- natural
+        return $ Star (fromInteger n)
 
-     boundTerm ∷ Parser ITerm
-     boundTerm =
-       do reserved "Bound"
-          index <- nats
-          return $ Bound (fromInteger index)
+piTerm ∷ Parser ITerm
+piTerm = 
+     do reserved "[Π]"
+        pi <- natw
+        input <- cterm
+        func <- cterm
+        return $ Pi pi input func
+        
+pmTerm ∷ Parser ITerm
+pmTerm = 
+  do reserved "[π]"
+     pm <- natw
+     input <- cterm
+     func <- cterm
+     return $ Pm pm input func
+     
+paTerm ∷ Parser ITerm
+paTerm = 
+  do reserved "/\\"
+     pa <- natw
+     input <- cterm
+     func <- cterm
+     return $ Pa pa input func
+     
+npmTerm :: Parser ITerm
+npmTerm =
+  do reserved "\\/"
+     input1 <- cterm
+     input2 <- cterm
+     return $ NPm input1 input2
 
-     --Parser for the global free variable name
-     gName ∷ Parser String
-     gName =  parens gName
-          <|> identifier
-     --Parser for Name data type
-     name ∷ Parser Name
-     name = do reserved "Local"
-               index <- nats
-               return $ Local (fromInteger index)
-        <|> do reserved "Quote"
-               index <- nats
-               return $ Quote (fromInteger index)
-        <|> do reserved "Global"
-               gname <- gName
-               return $ Global gname
+boundTerm ∷ Parser ITerm
+boundTerm =
+     do reserved "Bound"
+        index <- natural
+        return $ Bound (fromInteger index)
 
-     freeTerm ∷ Parser ITerm
-     freeTerm =
-       do reserved "Free"
-          fname <- name
-          return $ Free fname
+--Parser for the global free variable name
+gName ∷ Parser String
+gName =  parens gName
+     <|> identifier
+--Parser for Name data type
+name ∷ Parser Name
+name = do reserved "Local"
+          index <- natural
+          return $ Local (fromInteger index)
+     <|> do reserved "Quote"
+            index <- natural
+            return $ Quote (fromInteger index)
+     <|> do reserved "Global"
+            gname <- gName
+            return $ Global gname
 
-     appTerm ∷ Parser ITerm
-     appTerm =
-       do reserved "App"
-          pi <- nats
-          iterm <- term
-          cTerm <- cterm
-          eof
-          return $ App pi iterm cTerm
+freeTerm ∷ Parser ITerm
+freeTerm =
+     do reserved "Free"
+        fname <- name
+        return $ Free fname
 
-     iterm ∷ Parser ITerm
-     iterm =  appTerm --Application
-          <|> term --all ITerms except the application ITerm
+appTerm ∷ Parser ITerm
+appTerm =
+     do reserved "App"
+        pi <- natw
+        iterm <- term
+        cTerm <- cterm
+        eof
+        return $ App pi iterm cTerm
 
-     parseWhole ∷ Parser ITerm
-     parseWhole =
-       do whiteSpace
-          t <- iterm
-          whiteSpace
-          eof
-          return t
+parseWhole ∷ Parser a -> Parser a
+parseWhole p =
+     do whiteSpace
+        t <- p
+        whiteSpace
+        eof
+        return t
 
-     term ∷ Parser ITerm
-     term =  parens term
-        <|> appTerm
-     --ITerms with CTerms as input(s).
-        <|> piTerm
-        <|> boundTerm --Bound var
-        <|> freeTerm --Free var
+term ∷ Parser ITerm
+term = sortTerm
+     <|> appTerm
+     <|> piTerm
+     <|> pmTerm
+     <|> paTerm
+     <|> npmTerm
+     <|> boundTerm --Bound var
+     <|> freeTerm --Free var
 
-     cterm ∷ Parser CTerm
-     cterm =  parens cterm
-          <|> do reservedOp "Inf"
-                 iterm <- term
-                 return $ Inf iterm
-          <|> do reservedOp "Lam"
-                 pi <- nats
-                 cTerm <- cterm
-                 return $ Lam pi cTerm
+cterm ∷ Parser CTerm
+cterm =  do reservedOp "Inf"
+            iterm <- term
+            return $ Inf iterm
+     <|> do reservedOp "Lam"
+            pi <- natw
+            cTerm <- cterm
+            return $ Lam pi cTerm
 
-     parseString ∷ Parser a → String → a
-     parseString p str =
-       case parse p "" str of
-          Left e -> error $ show e
-          Right r -> r
+parseString ∷ Parser a → String → a
+parseString p str =
+     case parse p "" str of
+     Left e -> error $ show e
+     Right r -> r
 
--}
