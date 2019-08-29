d2b32e266d0cc3fa1fbdd75c7e81170e3c626ef4
Marty Stumpf <thealmartyblog@gmail.com>
2019-08-29 11:29:57 -0700

    Fix format.

--- src/Juvix/Core/MainLang.hs
+++ src/Juvix/Core/MainLang.hs
@@ -62,15 +62,15 @@ data Name
 data Value
   = VStar Natural
   | VNats
-  | VPi NatAndw Value (Value -> Value)
-  | VPm NatAndw Value (Value -> Value)
-  | VPa NatAndw Value (Value -> Value)
+  | VPi NatAndw Value (Value → Value)
+  | VPm NatAndw Value (Value → Value)
+  | VPa NatAndw Value (Value → Value)
   | VNPm Value Value
-  | VLam NatAndw (Value -> Value)
+  | VLam NatAndw (Value → Value)
   | VNeutral Neutral
   | VNat Natural
 
-showVal :: Value -> String
+showVal ∷ Value → String
 showVal (VLam _ f) = showFun f
 showVal (VStar i) = "*" ++ show i
 showVal VNats = "Nats"
@@ -82,7 +82,7 @@ showVal (VNPm _ _) = "\\/"
 showVal (VNeutral _n) = "neutral "
 showVal (VNat i) = show i
 
-showFun :: (Value -> Value) -> String
+showFun ∷ (Value → Value) → String
 showFun _f = "\\x.t"
 
 --A neutral term is either a variable or an application of a neutral term to a value
@@ -91,7 +91,7 @@ data Neutral
   | NApp Neutral Value
 
 --vfree creates the value corresponding to a free variable
-vfree :: Name -> Value
+vfree ∷ Name → Value
 vfree n = VNeutral (NFree n)
 
 --Annotations include usage and type.
@@ -103,7 +103,7 @@ type Context = [(Name, Annotation)]
 --Evaluation
 type Env = [Value]
 
-cEval :: CTerm -> Env -> Value
+cEval ∷ CTerm → Env → Value
 cEval (Star i) _d      = VStar i
 cEval Nats _d          = VNats
 cEval (Pi pi ty ty') d = VPi pi (cEval ty d) (\x -> cEval ty' (x : d))
@@ -113,17 +113,17 @@ cEval (NPm ty ty') d   = VNPm (cEval ty d) (cEval ty' d)
 cEval (Lam pi e) d     = VLam pi (\x -> cEval e (x : d))
 cEval (Conv ii) d      = iEval ii d
 
-toInt :: Natural -> Int
+toInt ∷ Natural → Int
 toInt = fromInteger . toInteger
 
-iEval :: ITerm -> Env -> Value
+iEval ∷ ITerm → Env → Value
 iEval (Free x) _d            = vfree x
 iEval (Bound ii) d           = d !! toInt ii --(!!) :: [a] -> Int -> a, the list lookup operator.
 iEval (Nat n) _d             = VNat n
 iEval (App iterm cterm) d    = vapp (iEval iterm d) (cEval cterm d)
 iEval (Ann _pi term _type) d = cEval term d
 
-vapp :: Value -> Value -> Value
+vapp ∷ Value → Value → Value
 vapp (VLam _pi f) v = f v
 vapp (VNeutral n) v = VNeutral (NApp n v)
 vapp x y =
@@ -132,7 +132,7 @@ vapp x y =
      showVal y ++ "\n to \n" ++ showVal x)
 
 --substitution function for checkable terms
-cSubst :: Natural -> ITerm -> CTerm -> CTerm
+cSubst ∷ Natural → ITerm → CTerm → CTerm
 cSubst _ii _r (Star i)     = Star i
 cSubst _ii _r Nats         = Nats
 cSubst ii r (Pi pi ty ty') = Pi pi (cSubst ii r ty) (cSubst (ii + 1) r ty')
@@ -143,7 +143,7 @@ cSubst ii r (Lam pi f)     = Lam pi (cSubst (ii + 1) r f)
 cSubst ii r (Conv e)       = Conv (iSubst ii r e)
 
 --substitution function for inferable terms
-iSubst :: Natural -> ITerm -> ITerm -> ITerm
+iSubst ∷ Natural → ITerm → ITerm → ITerm
 iSubst ii r (Bound j)
   | ii == j = r
   | otherwise = Bound j
@@ -154,10 +154,10 @@ iSubst ii r (Ann pi term t) = Ann pi (cSubst ii r term) (cSubst ii r t)
 
 --iSubst ii r (App iterm cterm) =
 --Quotation: takes a value back to a term
-quote0 :: Value -> CTerm
+quote0 ∷ Value → CTerm
 quote0 = quote 0
 
-quote :: Natural -> Value -> CTerm
+quote ∷ Natural → Value → CTerm
 quote _ii (VStar n) = Star n
 quote _ii VNats = Nats
 quote ii (VPi pi v f) =
@@ -171,17 +171,17 @@ quote ii (VLam pi f) = Lam pi (quote (ii + 1) (f (vfree (Quote ii))))
 quote ii (VNeutral n) = Conv (neutralQuote ii n)
 quote _ii (VNat n) = Conv (Nat n)
 
-neutralQuote :: Natural -> Neutral -> ITerm
+neutralQuote ∷ Natural → Neutral → ITerm
 neutralQuote ii (NFree x)  = boundfree ii x
 neutralQuote ii (NApp n v) = App (neutralQuote ii n) (quote ii v)
 
 --checks if the variable occurring at the head of the application is a bound variable or a free name
-boundfree :: Natural -> Name -> ITerm
+boundfree ∷ Natural → Name → ITerm
 boundfree ii (Quote k) = Bound (ii - k - 1)
 boundfree _ii x        = Free x
 
 --error message for inferring/checking types
-errorMsg :: Natural -> ITerm -> Annotation -> Annotation -> String
+errorMsg ∷ Natural → ITerm → Annotation → Annotation → String
 errorMsg binder iterm expectedT gotT =
   "Type mismatched. \n" ++
   show iterm ++
@@ -199,7 +199,7 @@ errorMsg binder iterm expectedT gotT =
 type Result a = Either String a --when type checking fails, it throws an error.
 
 --checkable terms take a type as input and returns ().
-cType :: Natural -> Context -> CTerm -> Annotation -> Result ()
+cType ∷ Natural → Context → CTerm → Annotation → Result ()
 cType ii g (Star n) v = undefined
 cType ii g (Conv e) v = do
   v' <- iType ii g e
@@ -222,10 +222,10 @@ cType ii _g cterm theType =
      showVal (snd theType) ++ " with " ++ show (fst theType) ++ "usage.")
 
 --inferable terms have type as output.
-iType0 :: Context -> ITerm -> Result Annotation
+iType0 ∷ Context → ITerm → Result Annotation
 iType0 = iType 0
 
-iType :: Natural -> Context -> ITerm -> Result Annotation
+iType ∷ Natural → Context → ITerm → Result Annotation
 iType ii g (Free x) =
   case lookup x g of
     Just ty -> return ty
