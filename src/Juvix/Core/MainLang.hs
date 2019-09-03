a824a3539748312caaa5fb93b8edcf2258273ba7
Marty Stumpf <thealmartyblog@gmail.com>
2019-09-03 12:10:26 -0700

    WIP type checker parsing, following cType signature. Switching to users input terms not values.

--- src/Juvix/Core/MainLang.hs
+++ src/Juvix/Core/MainLang.hs
@@ -63,15 +63,15 @@ data Name
 data Value
   = VStar Natural
   | VNats
-  | VPi NatAndw Value (Value → Value)
-  | VPm NatAndw Value (Value → Value)
-  | VPa NatAndw Value (Value → Value)
+  | VPi NatAndw Value (Value -> Value)
+  | VPm NatAndw Value (Value -> Value)
+  | VPa NatAndw Value (Value -> Value)
   | VNPm Value Value
-  | VLam NatAndw (Value → Value)
+  | VLam NatAndw (Value -> Value)
   | VNeutral Neutral
   | VNat Natural
 
-showVal ∷ Value → String
+showVal :: Value -> String
 showVal (VLam _ f) = showFun f
 showVal (VStar i) = "*" ++ show i
 showVal VNats = "Nats"
@@ -83,7 +83,7 @@ showVal (VNPm _ _) = "\\/"
 showVal (VNeutral _n) = "neutral "
 showVal (VNat i) = show i
 
-showFun ∷ (Value → Value) → String
+showFun :: (Value -> Value) -> String
 showFun _f = "\\x.t"
 
 --A neutral term is either a variable or an application of a neutral term to a value
@@ -92,7 +92,7 @@ data Neutral
   | NApp Neutral Value
 
 --vfree creates the value corresponding to a free variable
-vfree ∷ Name → Value
+vfree :: Name -> Value
 vfree n = VNeutral (NFree n)
 
 --Annotations include usage and type.
@@ -104,7 +104,7 @@ type Context = [(Name, Annotation)]
 --Evaluation
 type Env = [Value]
 
-cEval ∷ CTerm → Env → Value
+cEval :: CTerm -> Env -> Value
 cEval (Star i) _d      = VStar i
 cEval Nats _d          = VNats
 cEval (Pi pi ty ty') d = VPi pi (cEval ty d) (\x -> cEval ty' (x : d))
@@ -114,17 +114,17 @@ cEval (NPm ty ty') d   = VNPm (cEval ty d) (cEval ty' d)
 cEval (Lam pi e) d     = VLam pi (\x -> cEval e (x : d))
 cEval (Conv ii) d      = iEval ii d
 
-toInt ∷ Natural → Int
+toInt :: Natural -> Int
 toInt = fromInteger . toInteger
 
-iEval ∷ ITerm → Env → Value
+iEval :: ITerm -> Env -> Value
 iEval (Free x) _d            = vfree x
 iEval (Bound ii) d           = d !! toInt ii --(!!) :: [a] -> Int -> a, the list lookup operator.
 iEval (Nat n) _d             = VNat n
 iEval (App iterm cterm) d    = vapp (iEval iterm d) (cEval cterm d)
 iEval (Ann _pi term _type) d = cEval term d
 
-vapp ∷ Value → Value → Value
+vapp :: Value -> Value -> Value
 vapp (VLam _pi f) v = f v
 vapp (VNeutral n) v = VNeutral (NApp n v)
 vapp x y =
@@ -133,7 +133,7 @@ vapp x y =
      showVal y ++ "\n to \n" ++ showVal x)
 
 --substitution function for checkable terms
-cSubst ∷ Natural → ITerm → CTerm → CTerm
+cSubst :: Natural -> ITerm -> CTerm -> CTerm
 cSubst _ii _r (Star i)     = Star i
 cSubst _ii _r Nats         = Nats
 cSubst ii r (Pi pi ty ty') = Pi pi (cSubst ii r ty) (cSubst (ii + 1) r ty')
@@ -144,7 +144,7 @@ cSubst ii r (Lam pi f)     = Lam pi (cSubst (ii + 1) r f)
 cSubst ii r (Conv e)       = Conv (iSubst ii r e)
 
 --substitution function for inferable terms
-iSubst ∷ Natural → ITerm → ITerm → ITerm
+iSubst :: Natural -> ITerm -> ITerm -> ITerm
 iSubst ii r (Bound j)
   | ii == j = r
   | otherwise = Bound j
@@ -155,10 +155,10 @@ iSubst ii r (Ann pi term t) = Ann pi (cSubst ii r term) (cSubst ii r t)
 
 --iSubst ii r (App iterm cterm) =
 --Quotation: takes a value back to a term
-quote0 ∷ Value → CTerm
+quote0 :: Value -> CTerm
 quote0 = quote 0
 
-quote ∷ Natural → Value → CTerm
+quote :: Natural -> Value -> CTerm
 quote _ii (VStar n) = Star n
 quote _ii VNats = Nats
 quote ii (VPi pi v f) =
@@ -172,17 +172,17 @@ quote ii (VLam pi f) = Lam pi (quote (ii + 1) (f (vfree (Quote ii))))
 quote ii (VNeutral n) = Conv (neutralQuote ii n)
 quote _ii (VNat n) = Conv (Nat n)
 
-neutralQuote ∷ Natural → Neutral → ITerm
+neutralQuote :: Natural -> Neutral -> ITerm
 neutralQuote ii (NFree x)  = boundfree ii x
 neutralQuote ii (NApp n v) = App (neutralQuote ii n) (quote ii v)
 
 --checks if the variable occurring at the head of the application is a bound variable or a free name
-boundfree ∷ Natural → Name → ITerm
+boundfree :: Natural -> Name -> ITerm
 boundfree ii (Quote k) = Bound (ii - k - 1)
 boundfree _ii x        = Free x
 
 --error message for inferring/checking types
-errorMsg ∷ Natural → CTerm → Annotation → Annotation → String
+errorMsg :: Natural -> CTerm -> Annotation -> Annotation -> String
 errorMsg binder iterm expectedT gotT =
   "Type mismatched. \n" ++
   show iterm ++
@@ -190,21 +190,25 @@ errorMsg binder iterm expectedT gotT =
   show binder ++
   ") is of type \n" ++
   show (showVal (snd gotT)) ++
-  " , with" ++
+  " , with " ++
   show (fst gotT) ++
-  "usage.\n But the expected type is " ++
+  " usage.\n But the expected type is " ++
   show (showVal (snd expectedT)) ++
-  " , with" ++ show (fst expectedT) ++ "usage."
+  " , with " ++ show (fst expectedT) ++ " usage."
 
 --Type (and usage) checking
 type Result a = Either String a --when type checking fails, it throws an error.
 
 --checkable terms take a type as input and returns ().
-cType ∷ Natural → Context → CTerm → Annotation → Result ()
+cType :: Natural -> Context -> CTerm -> Annotation -> Result ()
 cType ii _g (Star n) v =
   unless
     (fst v == 0 && quote0 (snd v) == Star (n + 1))
     (throwError (errorMsg ii (Star n) (0, VStar (n + 1)) v))
+cType ii _g Nats v =
+  unless
+    (fst v == 0 && quote0 (snd v) == Star 0)
+    (throwError (errorMsg ii Nats (0, VStar 0) v))
 cType ii g (Conv e) v = do
   v' <- iType ii g e
   unless
@@ -226,10 +230,10 @@ cType ii _g cterm theType =
      showVal (snd theType) ++ " with " ++ show (fst theType) ++ "usage.")
 
 --inferable terms have type as output.
-iType0 ∷ Context → ITerm → Result Annotation
+iType0 :: Context -> ITerm -> Result Annotation
 iType0 = iType 0
 
-iType ∷ Natural → Context → ITerm → Result Annotation
+iType :: Natural -> Context -> ITerm -> Result Annotation
 iType ii g (Free x) =
   case lookup x g of
     Just ty -> return ty
--- src/Juvix/Core/Parser.hs
+++ src/Juvix/Core/Parser.hs
@@ -8,7 +8,7 @@ import           Text.ParserCombinators.Parsec
 import           Text.ParserCombinators.Parsec.Language
 import qualified Text.ParserCombinators.Parsec.Token    as Token
 
-languageDef ∷ GenLanguageDef String u Data.Functor.Identity.Identity
+languageDef :: GenLanguageDef String u Data.Functor.Identity.Identity
 languageDef =
   emptyDef
     { Token.commentStart = "/*"
@@ -28,34 +28,45 @@ languageDef =
         , "Quote"
         , "Global" --Free var
         , "w" --Omega
-        , "App"
+        , "App" --application
+        , "VStar" --Values
+        , "VNats"
+        , "VPi"
+        , "VPm"
+        , "VPa"
+        , "VNPm"
+        , "VLam"
+        , "VNeutral"
+        , "VNat"
+        , "NFree" --Neutral
+        , "NApp"
         ]
-    , Token.reservedOpNames = ["Conv", "\\", ":"]
+    , Token.reservedOpNames = ["Conv", "\\", ":", "cType"]
     }
 
-lexer ∷ Token.GenTokenParser String u Data.Functor.Identity.Identity
+lexer :: Token.GenTokenParser String u Data.Functor.Identity.Identity
 lexer = Token.makeTokenParser languageDef
      -- These are parsers for what their names signify
 
-identifier ∷ Parser String
+identifier :: Parser String
 identifier = Token.identifier lexer
 
-reserved ∷ String → Parser ()
+reserved :: String -> Parser ()
 reserved = Token.reserved lexer
 
-reservedOp ∷ String → Parser ()
+reservedOp :: String -> Parser ()
 reservedOp = Token.reservedOp lexer
 
-parens ∷ Parser a → Parser a
+parens :: Parser a -> Parser a
 parens = Token.parens lexer -- parses surrounding parenthesis, and what is inside them
 
-natural ∷ Parser Integer
+natural :: Parser Integer
 natural = Token.natural lexer
 
-whiteSpace ∷ Parser ()
+whiteSpace :: Parser ()
 whiteSpace = Token.whiteSpace lexer
 
-natw ∷ Parser NatAndw
+natw :: Parser NatAndw
 natw =
   do reserved "w"
      return Omega
@@ -63,13 +74,13 @@ natw =
     n <- natural
     return $ SNat (fromInteger n)
 
-sortTerm ∷ Parser CTerm
+sortTerm :: Parser CTerm
 sortTerm = do
   reserved "*"
   n <- natural
   return $ Star (fromInteger n)
 
-piTerm ∷ Parser CTerm
+piTerm :: Parser CTerm
 piTerm = do
   reserved "[Π]"
   pi <- natw
@@ -77,7 +88,7 @@ piTerm = do
   func <- ctermOnly
   return $ Pi pi input func
 
-pmTerm ∷ Parser CTerm
+pmTerm :: Parser CTerm
 pmTerm = do
   reserved "[π]"
   pm <- natw
@@ -85,7 +96,7 @@ pmTerm = do
   func <- ctermOnly
   return $ Pm pm input func
 
-paTerm ∷ Parser CTerm
+paTerm :: Parser CTerm
 paTerm = do
   reserved "/\\"
   pa <- natw
@@ -93,65 +104,65 @@ paTerm = do
   func <- ctermOnly
   return $ Pa pa input func
 
-npmTerm ∷ Parser CTerm
+npmTerm :: Parser CTerm
 npmTerm = do
   reserved "\\/"
   fst <- ctermOnly
   snd <- ctermOnly
   return $ NPm fst snd
 
-lamTerm ∷ Parser CTerm
+lamTerm :: Parser CTerm
 lamTerm = do
   reservedOp "\\"
   pi <- natw
   func <- ctermOnly
   return $ Lam pi func
 
-convTerm ∷ Parser CTerm
+convTerm :: Parser CTerm
 convTerm = do
   reservedOp "Conv"
   termToConvert <- iterm
   return $ Conv termToConvert
 
-boundTerm ∷ Parser ITerm
+boundTerm :: Parser ITerm
 boundTerm = do
   reserved "Bound"
   index <- natural
   return $ Bound (fromInteger index)
 
 --Parser for the global free variable name
-gName ∷ Parser String
+gName :: Parser String
 gName = parens gName <|> identifier
 
 --Parser for Name data type
-localTerm ∷ Parser Name
+localTerm :: Parser Name
 localTerm = do
   reserved "Local"
   index <- natural
   return $ Local (fromInteger index)
 
-quoteTerm ∷ Parser Name
+quoteTerm :: Parser Name
 quoteTerm = do
   reserved "Quote"
   index <- natural
   return $ Quote (fromInteger index)
 
-globalTerm ∷ Parser Name
+globalTerm :: Parser Name
 globalTerm = do
   reserved "Global"
   gname <- gName
   return $ Global gname
 
-name ∷ Parser Name
-name = localTerm <|> quoteTerm <|> globalTerm
+pName :: Parser Name
+pName = localTerm <|> quoteTerm <|> globalTerm
 
-freeTerm ∷ Parser ITerm
+freeTerm :: Parser ITerm
 freeTerm = do
   reserved "Free"
-  fname <- name
+  fname <- pName
   return $ Free fname
 
-appTerm ∷ Parser ITerm
+appTerm :: Parser ITerm
 appTerm = do
   reserved "App"
   func <- iterm
@@ -159,7 +170,7 @@ appTerm = do
   eof
   return $ App func var
 
-annTerm ∷ Parser ITerm
+annTerm :: Parser ITerm
 annTerm = do
   pi <- natw
   term <- ctermOnly
@@ -168,7 +179,7 @@ annTerm = do
   eof
   return $ Ann pi term ann
 
-parseWhole ∷ Parser a → Parser a
+parseWhole :: Parser a -> Parser a
 parseWhole p = do
   whiteSpace
   t <- p
@@ -176,20 +187,20 @@ parseWhole p = do
   eof
   return t
 
-cterm ∷ Parser CTerm
+cterm :: Parser CTerm
 cterm =
   parens cterm <|> sortTerm <|> piTerm <|> pmTerm <|> paTerm <|> npmTerm <|>
   lamTerm <|>
   convTerm <|>
   ctermOnly
 
-iterm ∷ Parser ITerm
+iterm :: Parser ITerm
 iterm = parens iterm <|> boundTerm <|> freeTerm <|> appTerm <|> annTerm
 
-cOriTerm ∷ Parser (Either ITerm CTerm)
+cOriTerm :: Parser (Either ITerm CTerm)
 cOriTerm = Text.Parsec.try (Left <$> iterm) <|> (Right <$> cterm)
 
-ctermOnly ∷ Parser CTerm
+ctermOnly :: Parser CTerm
 ctermOnly = do
   anyTerm <- cOriTerm
   return
@@ -197,7 +208,71 @@ ctermOnly = do
        Left i  -> (Conv i)
        Right c -> c)
 
-parseString ∷ Parser a → String → Maybe a
+vStar :: Parser Value
+vStar = do
+  reserved "VStar"
+  i <- natural
+  return VStar i
+
+vNats :: Parser Value
+vNats = do
+  reserved "VNats"
+  return VNats
+
+{-
+vPi :: Parser Value
+vPi = do
+  reserved "VPi"
+  usage <- natw
+  var <- pValue
+  func <-
+-}
+neutral :: Parser Neutral
+neutral =
+  do reserved "NFree"
+     freeName <- name
+     return NFree freeName
+     <|> do
+    reserved "NApp"
+    var <- neutral
+    func <- pValue
+    return NApp var func
+
+vNeutral :: Parser Value
+vNeutral = do
+  reserved "VNeutral"
+
+pValue :: Parser Value
+pValue = vStar <|> vNats <|> vLam <|> vNeutral <|> vNat
+
+-- <|> vPi <|> vPm <|> vPa <|> vNpm
+pAnnotation :: Parser Annotation --Annotation = (NatAndw, Value)
+pAnnotation = do
+  usage <- natw
+  Parsec.spaces
+  typeValue <- pValue
+  return (usage, typeValue)
+
+pContext :: Parser Context --Context = [(Name, Annotation)]
+pContext = do
+  varName <- pName
+  Parsec.spaces
+  varAnn <- pAnnotation
+  return (varName, varAnn)
+
+--Parses type checker input
+pCType :: Parser Either String ()
+pCType = do
+  reservedOp "cType"
+  index <- natural
+  context <- pContext
+  cterm <- ctermOnly
+  return 
+    case cType index context cterm of
+      Left msg -> return putStr msg
+      Right _ -> return _
+
+parseString :: Parser a -> String -> Maybe a
 parseString p str =
   case parse p "" str of
     Left _  -> Nothing
