db5ce1311f7c97a683e210ba088a966691127735
Marty Stumpf <thealmartyblog@gmail.com>
2019-09-05 12:33:55 -0700

    Abstract out semiring choice, named as usage.

--- src/Juvix/Core/MainLang.hs
+++ src/Juvix/Core/MainLang.hs
@@ -6,6 +6,9 @@ import           Control.Monad.Except
 import           Numeric.Natural
 import           Prelude
 
+-- naming usage for easy change of semiring choice.
+type Usage = NatAndw
+
 data NatAndw -- semiring of (Nat,w) for usage annotation
   = SNat Natural -- 0, 1, or n usage
   | Omega -- unspecified usage
@@ -32,13 +35,13 @@ instance Num NatAndw where
 data CTerm
   = Star Natural -- (sort i) i th ordering of (closed) universe.
   | Nats -- (Prim) primitive type (naturals)
-  | Pi NatAndw CTerm CTerm -- formation rule of the dependent function type (PI).
-                              -- the NatAndw(π) tracks how many times x is used.
-  | Pm NatAndw CTerm CTerm -- dependent multiplicative conjunction (tensor product)
-  | Pa NatAndw CTerm CTerm -- dependent additive conjunction type
+  | Pi Usage CTerm CTerm -- formation rule of the dependent function type (PI).
+                              -- the Usage(π) tracks how many times x is used.
+  | Pm Usage CTerm CTerm -- dependent multiplicative conjunction (tensor product)
+  | Pa Usage CTerm CTerm -- dependent additive conjunction type
   | NPm CTerm CTerm -- non-dependent multiplicative disjunction type
-  | Lam NatAndw CTerm --(LAM) Introduction rule of PI.
-                        -- The abstracted variable's usage is tracked with the NatAndw(π).
+  | Lam Usage CTerm --(LAM) Introduction rule of PI.
+                        -- The abstracted variable's usage is tracked with the Usage(π).
   | Conv ITerm --(CONV) conversion rule. TODO make sure 0Γ ⊢ S≡T
               -- Conv is the constructor that embeds ITerm to CTerm
   deriving (Show, Eq)
@@ -49,8 +52,8 @@ data ITerm
   | Free Name -- Free variables of type name (see below)
   | Nat Natural -- primitive constant (naturals)
   | App ITerm CTerm -- elimination rule of PI (APP).
-                              -- the NatAndw(π) tracks how x is use.
-  | Ann NatAndw CTerm CTerm --Annotation with usage.
+                              -- the Usage(π) tracks how x is use.
+  | Ann Usage CTerm CTerm --Annotation with usage.
   deriving (Show, Eq)
 
 data Name
@@ -63,15 +66,15 @@ data Name
 data Value
   = VStar Natural
   | VNats
-  | VPi NatAndw Value (Value → Value)
-  | VPm NatAndw Value (Value → Value)
-  | VPa NatAndw Value (Value → Value)
+  | VPi Usage Value (Value -> Value)
+  | VPm Usage Value (Value -> Value)
+  | VPa Usage Value (Value -> Value)
   | VNPm Value Value
-  | VLam NatAndw (Value → Value)
+  | VLam Usage (Value -> Value)
   | VNeutral Neutral
   | VNat Natural
 
-showVal ∷ Value → String
+showVal :: Value -> String
 showVal (VLam _ f) = showFun f
 showVal (VStar i) = "*" ++ show i
 showVal VNats = "Nats"
@@ -83,7 +86,7 @@ showVal (VNPm _ _) = "\\/"
 showVal (VNeutral _n) = "neutral "
 showVal (VNat i) = show i
 
-showFun ∷ (Value → Value) → String
+showFun :: (Value -> Value) -> String
 showFun _f = "\\x.t"
 
 --A neutral term is either a variable or an application of a neutral term to a value
@@ -92,11 +95,11 @@ data Neutral
   | NApp Neutral Value
 
 --vfree creates the value corresponding to a free variable
-vfree ∷ Name → Value
+vfree :: Name -> Value
 vfree n = VNeutral (NFree n)
 
 --Annotations include usage and type.
-type Annotation = (NatAndw, Value)
+type Annotation = (Usage, Value)
 
 --Contexts map variables to their types.
 type Context = [(Name, Annotation)]
@@ -104,7 +107,7 @@ type Context = [(Name, Annotation)]
 --Evaluation
 type Env = [Value]
 
-cEval ∷ CTerm → Env → Value
+cEval :: CTerm -> Env -> Value
 cEval (Star i) _d      = VStar i
 cEval Nats _d          = VNats
 cEval (Pi pi ty ty') d = VPi pi (cEval ty d) (\x -> cEval ty' (x : d))
@@ -114,17 +117,17 @@ cEval (NPm ty ty') d   = VNPm (cEval ty d) (cEval ty' d)
 cEval (Lam pi e) d     = VLam pi (\x -> cEval e (x : d))
 cEval (Conv ii) d      = iEval ii d
 
-toInt ∷ Natural → Int
+toInt :: Natural -> Int
 toInt = fromInteger . toInteger
 
-iEval ∷ ITerm → Env → Value
+iEval :: ITerm -> Env -> Value
 iEval (Free x) _d            = vfree x
 iEval (Bound ii) d           = d !! toInt ii --(!!) :: [a] -> Int -> a, the list lookup operator.
 iEval (Nat n) _d             = VNat n
 iEval (App iterm cterm) d    = vapp (iEval iterm d) (cEval cterm d)
 iEval (Ann _pi term _type) d = cEval term d
 
-vapp ∷ Value → Value → Value
+vapp :: Value -> Value -> Value
 vapp (VLam _pi f) v = f v
 vapp (VNeutral n) v = VNeutral (NApp n v)
 vapp x y =
@@ -133,7 +136,7 @@ vapp x y =
      showVal y ++ "\n to \n" ++ showVal x)
 
 --substitution function for checkable terms
-cSubst ∷ Natural → ITerm → CTerm → CTerm
+cSubst :: Natural -> ITerm -> CTerm -> CTerm
 cSubst _ii _r (Star i)     = Star i
 cSubst _ii _r Nats         = Nats
 cSubst ii r (Pi pi ty ty') = Pi pi (cSubst ii r ty) (cSubst (ii + 1) r ty')
@@ -144,7 +147,7 @@ cSubst ii r (Lam pi f)     = Lam pi (cSubst (ii + 1) r f)
 cSubst ii r (Conv e)       = Conv (iSubst ii r e)
 
 --substitution function for inferable terms
-iSubst ∷ Natural → ITerm → ITerm → ITerm
+iSubst :: Natural -> ITerm -> ITerm -> ITerm
 iSubst ii r (Bound j)
   | ii == j = r
   | otherwise = Bound j
@@ -155,10 +158,10 @@ iSubst ii r (Ann pi term t) = Ann pi (cSubst ii r term) (cSubst ii r t)
 
 --iSubst ii r (App iterm cterm) =
 --Quotation: takes a value back to a term
-quote0 ∷ Value → CTerm
+quote0 :: Value -> CTerm
 quote0 = quote 0
 
-quote ∷ Natural → Value → CTerm
+quote :: Natural -> Value -> CTerm
 quote _ii (VStar n) = Star n
 quote _ii VNats = Nats
 quote ii (VPi pi v f) =
@@ -172,17 +175,17 @@ quote ii (VLam pi f) = Lam pi (quote (ii + 1) (f (vfree (Quote ii))))
 quote ii (VNeutral n) = Conv (neutralQuote ii n)
 quote _ii (VNat n) = Conv (Nat n)
 
-neutralQuote ∷ Natural → Neutral → ITerm
+neutralQuote :: Natural -> Neutral -> ITerm
 neutralQuote ii (NFree x)  = boundfree ii x
 neutralQuote ii (NApp n v) = App (neutralQuote ii n) (quote ii v)
 
 --checks if the variable occurring at the head of the application is a bound variable or a free name
-boundfree ∷ Natural → Name → ITerm
+boundfree :: Natural -> Name -> ITerm
 boundfree ii (Quote k) = Bound (ii - k - 1)
 boundfree _ii x        = Free x
 
 --error message for inferring/checking types
-errorMsg ∷ Natural → CTerm → Annotation → Annotation → String
+errorMsg :: Natural -> CTerm -> Annotation -> Annotation -> String
 errorMsg binder iterm expectedT gotT =
   "Type mismatched. \n" ++
   show iterm ++
@@ -200,11 +203,11 @@ errorMsg binder iterm expectedT gotT =
 type Result a = Either String a --when type checking fails, it throws an error.
 
 --Usage comparisions
-usageCompare ∷ NatAndw → NatAndw → Bool
+usageCompare :: Usage -> Usage -> Bool
 usageCompare (SNat 0) pi = pi == SNat 0 || pi == Omega
 
 --checker for checkable terms checks the term against an annotation and returns ().
-cType ∷ Natural → Context → CTerm → Annotation → Result ()
+cType :: Natural -> Context -> CTerm -> Annotation -> Result ()
 cType ii _g (Star n) ann =
   unless -- TODO i only needs to be < j in typing rule?
     (usageCompare (SNat 0) (fst ann) && quote0 (snd ann) == Star (n + 1))
@@ -246,15 +249,15 @@ cType ii _g cterm theType =
      showVal (snd theType) ++ " with " ++ show (fst theType) ++ "usage.")
 
 --inferable terms have type as output.
-iType0 ∷ Context → ITerm → Result Value
+iType0 :: Context -> ITerm -> Result Value
 iType0 = iType 0
 
-iTypeErrorMsg ∷ Natural → Name → String
+iTypeErrorMsg :: Natural -> Name -> String
 iTypeErrorMsg ii x =
   "Cannot find the type of \n" ++
   show x ++ "\n (binder number " ++ show ii ++ ") in the environment."
 
-iType ∷ Natural → Context → ITerm → Result Value
+iType :: Natural -> Context -> ITerm -> Result Value
 iType ii g (Free x) =
   case lookup x g of
     Just ann -> return $ snd ann
