660c8686161fe769e132f8ca30dd1c72ecf5f296
Marty Stumpf <thealmartyblog@gmail.com>
2019-10-31 12:11:07 -0700

    Add unit type.

---
+++ src/Juvix/Core/Parameterisations/Unit.hs
@@ -0,0 +1,45 @@
+module Juvix.Core.Parameterisations.Unit where
+
+import Juvix.Core.Types hiding (apply, parseTy, parseVal, reservedNames, reservedOpNames, typeOf)
+import Juvix.Library hiding ((<|>))
+import Text.ParserCombinators.Parsec
+import qualified Text.ParserCombinators.Parsec.Token as Token
+import Prelude (String)
+
+-- k: primitive type: naturals
+data UnitTy
+  = TUnit
+  deriving (Show, Eq)
+
+-- c: primitive constant and f: functions
+data UnitVal
+  = Unit
+  deriving (Show, Eq)
+
+typeOf ∷ ∀ a. ([UnitTy] → a) → UnitVal → Either a UnitTy
+typeOf _ Unit = Right Unit
+
+apply ∷ UnitVal → UnitVal → Maybe UnitVal
+apply _ _ = Nothing
+
+parseTy ∷ Token.GenTokenParser String () Identity → Parser UnitTy
+parseTy lexer = do
+  Token.reserved lexer "TUnit"
+  pure TUnit
+
+parseVal ∷ Token.GenTokenParser String () Identity → Parser UnitVal
+parseVal lexer = do
+  Token.reserved lexer "Unit"
+  pure Unit
+
+parseUnit ∷ Token.GenTokenParser String () Identity → Parser UnitVal
+parseUnit lexer = Unitural . fromIntegral |<< Token.natural lexer
+
+reservedNames ∷ [String]
+reservedNames = ["TUnit", "Unit"]
+
+reservedOpNames ∷ [String]
+reservedOpNames = []
+
+unit ∷ Parameterisation UnitTy UnitVal
+unit = Parameterisation typeOf apply parseTy parseVal reservedNames reservedOpNames
