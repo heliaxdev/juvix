eb61fd552637d29880ce6d88cc1c4b53a1a3718f
Marty Stumpf <marty@cryptium.ch>
2019-11-08 10:48:05 -0800

    Merge PR #156: Add more type checker test cases

--- test/CoreTypechecker.hs
+++ test/CoreTypechecker.hs
@@ -56,6 +56,98 @@ identityApplication =
 natTy ∷ NatAnnotation
 natTy = (SNat 1, IR.VPrimTy Nat)
 
+-- (I:(Nat->Nat)->(Nat->Nat) I:(Nat->Nat)) I:Nat type checked to NatTy
+identityAppINat1 ∷ NatElim
+identityAppINat1 =
+  IR.App
+    ( IR.App
+        ( IR.Ann
+            (SNat 1)
+            identity
+            ( IR.Pi
+                (SNat 1)
+                (IR.Pi (SNat 1) (IR.PrimTy Nat) (IR.PrimTy Nat))
+                (IR.Pi (SNat 1) (IR.PrimTy Nat) (IR.PrimTy Nat))
+            )
+        )
+        ( IR.Elim
+            ( IR.Ann
+                (SNat 1)
+                identity
+                (IR.Pi (SNat 1) (IR.PrimTy Nat) (IR.PrimTy Nat))
+            )
+        )
+    )
+    (IR.Elim (IR.Prim (Natural 1)))
+
+-- I:(Nat->Nat)->(Nat->Nat) I:(Nat->Nat) type checked to (Nat->Nat)
+-- I:(Nat->Nat) I:(Nat->Nat) correctly does not type checked
+identityAppI ∷ NatElim
+identityAppI =
+  IR.App
+    ( IR.Ann
+        (SNat 1)
+        identity
+        ( IR.Pi
+            (SNat 1)
+            (IR.Pi (SNat 1) (IR.PrimTy Nat) (IR.PrimTy Nat))
+            (IR.Pi (SNat 1) (IR.PrimTy Nat) (IR.PrimTy Nat))
+        )
+    )
+    ( IR.Elim
+        ( IR.Ann
+            (SNat 1)
+            identity
+            (IR.Pi (SNat 1) (IR.PrimTy Nat) (IR.PrimTy Nat))
+        )
+    )
+
+kcombinator ∷ ∀ primTy primVal. IR.Term primTy primVal -- K = \x.\y.x
+kcombinator = IR.Lam (IR.Lam (IR.Elim (IR.Bound 1)))
+
+-- Nat -> (Nat -> Nat)
+kCompTy ∷ NatAnnotation
+kCompTy =
+  ( SNat 1,
+    IR.VPi
+      (SNat 1)
+      (IR.VPrimTy Nat)
+      (const (IR.VPi (SNat 0) (IR.VPrimTy Nat) (const (IR.VPrimTy Nat))))
+  )
+
+-- I:(Nat->Nat->Nat)->(Nat->Nat->Nat) K:(Nat->Nat->Nat) should type check to (Nat->Nat->Nat)
+identityAppK ∷ NatElim
+identityAppK =
+  IR.App
+    ( IR.Ann
+        (SNat 1)
+        identity
+        ( IR.Pi
+            (SNat 1)
+            ( IR.Pi
+                (SNat 1)
+                (IR.PrimTy Nat)
+                (IR.Pi (SNat 0) (IR.PrimTy Nat) (IR.PrimTy Nat))
+            )
+            ( IR.Pi
+                (SNat 1)
+                (IR.PrimTy Nat)
+                (IR.Pi (SNat 0) (IR.PrimTy Nat) (IR.PrimTy Nat))
+            )
+        )
+    )
+    ( IR.Elim
+        ( IR.Ann
+            (SNat 1)
+            kcombinator
+            ( IR.Pi
+                (SNat 1)
+                (IR.PrimTy Nat)
+                (IR.Pi (SNat 0) (IR.PrimTy Nat) (IR.PrimTy Nat))
+            )
+        )
+    )
+
 test_identity_computational ∷ T.TestTree
 test_identity_computational = shouldCheck nat identity identityNatCompTy
 
@@ -67,6 +159,18 @@ test_identity_contemplation = shouldCheck nat identity identityNatContTy
 
 test_identity_application ∷ T.TestTree
 test_identity_application = shouldCheck nat identityApplication natTy
+
+test_identity_app_I_Nat1 ∷ T.TestTree
+test_identity_app_I_Nat1 = shouldInfer nat identityAppINat1 natTy
+
+test_identity_app_I ∷ T.TestTree
+test_identity_app_I = shouldInfer nat identityAppI identityNatCompTy
+
+test_kcombinator_computational ∷ T.TestTree
+test_kcombinator_computational = shouldCheck nat kcombinator kCompTy
+
+test_identity_app_k ∷ T.TestTree
+test_identity_app_k = shouldInfer nat identityAppK kCompTy
 
 test_nats_type_star0 ∷ T.TestTree
 test_nats_type_star0 = shouldCheck nat (IR.PrimTy Nat) (SNat 0, IR.VStar 0)
@@ -170,18 +274,3 @@ twoCompTy =
       (IR.VPi (SNat 1) (IR.VPrimTy Nat) (const (IR.VPrimTy Nat)))
       (const (IR.VPi (SNat 1) (IR.VPrimTy Nat) (const (IR.VPrimTy Nat))))
   )
--- property tests of type checker:
--- the term's inferred type equals to the input type
--- property test of evaluator:
--- \x.x (any term) evaluates to (any term evaluated)
-{- lamProp ∷ Term → Env → Property
-lamProp cterm env = IR.App (cEval (IR.Lam IR.Bound 0) env) cterm == cterm-}
--- any constant term evaluates to itself
--- constProp ∷ Term → Env → Bool
--- constProp (Star i) env = cEval (Star i) env == IR.VStar i
--- constProp IR.PrimTy Nat env     = cEval IR.PrimTy Nat env == IR.VPrimTy Nat
--- constProp _ _          = True --Not testing non-const terms
-{- TODO need to combine generators to generate
-   Terms http://hackage.haskell.org/package/QuickCheck-2.13.2/docs/Test-QuickCheck-Gen.html
-instance Arbitrary Term where
-  arbitrary = Term -}
