dc85ae16719553b36f7c2010bb2b1c8837f4ee0e
mariari <32526923+mariari@users.noreply.github.com>
2020-09-22 10:14:38 -0400

    Initial substitution algorithm (#478)
    * started work on subst
    
    * made more comments
    
    * Started the subst algorithm
    
    * finished the main subst function
    
    * finished the initial algorithm
    
    * another commit to see if it'll update github
    
    * removed unused function
    
    * add an pre-inlining step
    
    * wrote a comment on inlineP
    
    * Update src/Juvix/Core/IR/Subst.hs
    
    Co-authored-by: Christopher Goes <cwgoes@pluranimity.org>
    
    * moved over the code to HR
    
    * moved over the algorithm to not be on IR
    
    * added the inscope set to the data structure and made unique names
    
    * finished the trenalsation over to the proper rapier technique
    
    Co-authored-by: Christopher Goes <cwgoes@pluranimity.org>

---
+++ src/Juvix/Core/HR/Subst.hs
@@ -0,0 +1,129 @@
+-- |
+-- - Runs a substitution algorithm over core
+module Juvix.Core.HR.Subst where
+
+import Control.Lens hiding ((|>))
+import qualified Data.HashSet as Set
+import qualified Juvix.Core.Common.NameSymbol as NameSymbol
+import qualified Juvix.Core.HR.Types as Types
+import qualified Juvix.Core.Usage as Usage
+import Juvix.Library
+import qualified Juvix.Library.HashMap as Map
+
+-- eventually a lot of this code will be replaced by a proper
+-- inliner strategy, but this code will serve as the basis for
+-- said iterative design
+
+data Key
+  = Bound Natural
+  | Global NameSymbol.T
+  deriving (Show, Eq, Generic)
+
+instance Hashable Key
+
+data T primTy primVal
+  = T
+      { _seenSet :: InScopeSet,
+        _sub :: Map.T Symbol (Types.Elim primTy primVal)
+      }
+  deriving (Show)
+
+type InScopeSet = Set.HashSet Symbol
+
+makeLenses ''T
+
+-- TODO ∷
+-- - add a context for this to run with
+-- - Turn the Context, T, InScopeSet to being in the Env
+-- - when seeing a new binding determine if we should add that to the Τ type
+--   + For now we just ignore extra values to inline, and continue on our way
+f ::
+  T primTy primVal -> Types.Term primTy primVal -> Types.Term primTy primVal
+f _ (Types.PrimTy ty) =
+  Types.PrimTy ty
+f _ (Types.Star uni) =
+  Types.Star uni
+f subst (Types.Pi usage name typ body) =
+  let (newSubst, newName) = uniqueNameAndUpdateMap subst name
+   in Types.Pi usage newName typ (f newSubst body)
+f subst (Types.Lam name body) =
+  let (newSubst, newName) = uniqueNameAndUpdateMap subst name
+   in Types.Lam newName (f newSubst body)
+f subst (Types.Let usage name bound body)
+  -- lets are non recrusive!
+  | inlineP usage bound =
+    let newBound =
+          substElim subst bound
+        newSubst =
+          subst
+            |> over sub (Map.insert name newBound)
+            |> over seenSet (Set.insert name)
+     in f newSubst body
+  | otherwise =
+    let (newSubst, newName) = uniqueNameAndUpdateMap subst name
+     in Types.Let usage newName (substElim subst bound) (f newSubst body)
+f subst (Types.Elim elim) =
+  Types.Elim (substElim subst elim)
+
+substElim ::
+  T primTy primVal -> Types.Elim primTy primVal -> Types.Elim primTy primVal
+substElim subst (Types.Var v) =
+  case (subst ^. sub) Map.!? v of
+    -- we only do pre-inlining currently...
+    -- need to think how to do post inline techniques
+    Just el -> el
+    Nothing -> Types.Var v
+substElim _ (Types.Prim prim) =
+  Types.Prim prim
+substElim subst (Types.App fun arg) =
+  Types.App (substElim subst fun) (f subst arg)
+substElim subst (Types.Ann usage ann term uni) =
+  Types.Ann usage (f subst ann) (f subst term) uni
+
+-- TODO ∷
+-- - take an environment of some kind
+--   + This env should have meta data at some point to better make
+--     decisions. Or better yet tag it to the structure itself in some
+--     extension.
+-- - Make this run on some type of size and usage measurement.
+--   + The current metric just checks if the usage is one. However
+--     this is bad, as we could move the evaluation in the middle of
+--     some computation that degrades performance.
+
+-- | 'inlineP' determines if a function should be inlined
+inlineP :: Usage.T -> Types.Elim primTy primVal -> Bool
+inlineP usage _
+  | usage == one = True
+  | otherwise = False
+
+empty :: T primTy primVal
+empty = T mempty mempty
+
+-- | 'uniqueNameAndUpdateMap' takes a Subst data structure and a symbol
+-- and updates the map to redirect symbols labeled x if it already seen
+uniqueNameAndUpdateMap :: T primTy primVal -> Symbol -> (T primTy primVal, Symbol)
+uniqueNameAndUpdateMap subst sym
+  | sym == newSym =
+    (newSubst |> over sub (Map.delete sym), newSym)
+  | otherwise =
+    (newSubst |> over sub (Map.insert sym (Types.Var newSym)), newSym)
+  where
+    (newSet, newSym) =
+      uniqueName sym (subst ^. seenSet)
+    newSubst =
+      subst |> set seenSet newSet
+
+-- TODO ∷ make a unique name when the hash value is the same name
+-- as a value in the hash itself... this shouldn't happen, but
+-- we need the linear add one strategy when this does occur
+
+-- | 'uniqueName' generates a unique name based off the symbol
+-- and the set
+uniqueName :: Symbol -> InScopeSet -> (InScopeSet, Symbol)
+uniqueName sym set
+  | Set.member sym set =
+    (Set.insert newSym set, newSym)
+  | otherwise =
+    (Set.insert sym set, sym)
+  where
+    newSym = intern (show (hash set)) <> sym
