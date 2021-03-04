33d837e588ec32e8472989822fd2e99c01dc22ad
Andy Morris <andy@cryptium.ch>
2021-03-04 14:21:22 +0100

    Remove the `PrettyPrec` type family
    Thinking about it, it will probably just cause problems if nested
    subterms actually start using a different precedence type

--- library/StandardLibrary/src/Juvix/Library/PrettyPrint.hs
+++ library/StandardLibrary/src/Juvix/Library/PrettyPrint.hs
@@ -9,39 +9,33 @@ import Text.PrettyPrint.Compact
 import Juvix.Library
 
 
+-- | Annotations, which can be used for e.g. syntax highlighting
 type family PrettyAnn a :: *
+
+type Prec = Natural
 
 -- | Class for pretty-printing syntax-like types, which need to keep track of
 -- the surrounding precedence level.
 class Monoid (PrettyAnn a) => PrettySyntax a where
-  -- | The type of precedence levels. Defaults to 'Natural'.
-  type PrettyPrec a
-  type PrettyPrec a = Natural
-
-  -- | The initial precedence level. Defaults to @0@.
-  initPrec :: PrettyPrec a
-  default initPrec :: Num (PrettyPrec a) => PrettyPrec a
-  initPrec = 0
-
   -- | Pretty-prints a syntax value, given the current precedence value in
   -- a reader environment.
   prettyPrec' ::
-    HasReader "prec" (PrettyPrec a) m =>
+    HasReader "prec" Prec m =>
     a -> m (Doc (PrettyAnn a))
   default prettyPrec' ::
-    (PrettyText a, HasReader "prec" (PrettyPrec a) m) =>
+    (PrettyText a, HasReader "prec" Prec m) =>
     a -> m (Doc (PrettyAnn a))
   prettyPrec' = pure . prettyText
 
 -- | Pretty-print at the given precedence level.
-prettyPrec :: PrettySyntax a => PrettyPrec a -> a -> Doc (PrettyAnn a)
+prettyPrec :: PrettySyntax a => Prec -> a -> Doc (PrettyAnn a)
 prettyPrec prec x =
   let MonadReader act = prettyPrec' x in
   runReader act prec
 
 -- | Pretty-print at the initial precedence level.
-prettyPrec0 :: forall a. PrettySyntax a => a -> Doc (PrettyAnn a)
-prettyPrec0 = prettyPrec (initPrec @a)
+prettyPrec0 :: PrettySyntax a => a -> Doc (PrettyAnn a)
+prettyPrec0 = prettyPrec 0
 
 -- | Class for text-like types (e.g. messages), which don't have a concept of
 -- precedence.
