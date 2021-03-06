f8a199ea97754350f1dde55ee4fd6f7bddd68b50
Andy Morris <andy@cryptium.ch>
2021-03-06 08:04:10 +0100

    reexport names etc from HR.Types

--- library/Core/src/Juvix/Core/HR/Types.hs
+++ library/Core/src/Juvix/Core/HR/Types.hs
@@ -1,11 +1,20 @@
-module Juvix.Core.HR.Types where
+module Juvix.Core.HR.Types
+  ( module Juvix.Core.HR.Types,
+    Universe,
+    GlobalName,
+    PatternVar,
+    PatternMap,
+    BoundVar,
+    Name (..),
+    GlobalUsage (..),
+  ) where
 
 import Juvix.Core.HR.Extend
-import qualified Juvix.Core.IR.Types.Base as IR
+import Juvix.Core.IR.Types.Base
 
 data T
 
-IR.extendTerm "Term" [] [t|T|] extTerm
+extendTerm "Term" [] [t|T|] extTerm
 
 -- TODO allow extendTerm to reorder fields?
 pattern Lam x t = Lam0 t x
@@ -18,4 +27,4 @@ pattern Let π x l b = Let0 π l b x
 
 {-# COMPLETE Star, PrimTy, Prim, Pi, Lam, Sig, Pair, Let, UnitTy, Unit, Elim #-}
 
-IR.extendElim "Elim" [] [t|T|] extElim
+extendElim "Elim" [] [t|T|] extElim
